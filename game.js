b8a8da34f08de321d14d540280f4848907512b6f
Ahnaf <ahnafanis0@gmail.com>
2022-06-26 00:19:12 +0600

    bug fix

--- game.js
+++ game.js
@@ -1,35 +1,39 @@
 "use strict";
 function computerPlay() {
-	const moveset = ["rock", "paper", "scissors"];
-	const move = moveset[(Math.random() * moveset.length) | 0];
-	return move;
+    const moveset = ["rock", "paper", "scissors"];
+    const move = moveset[(Math.random() * moveset.length)|0];
+    return move;
 }
-function playerPlay() {
-	const question = prompt("Rock, Paper or Scissors?");
-	const move = question.toLowerCase();
-	return move;
+function playerPlay() { 
+   const question = prompt("Rock, Paper or Scissors?");
+    const move = question.toLowerCase();
+    return move;
 }
 function game(playerMove, computerMove) {
-	if (
-		(playerMove === "paper" && computerMove === "scissors") ||
-		(playerMove === "scissors" && computerMove === "rock") ||
-		(playerMove === "rock" && computerMove === "paper")
-	) {
-		return "You Lose! ";
-	} else if (
-		playerMove !== "paper" &&
-		playerMove !== "scissors" &&
-		playerMove !== "rock"
-	) {
-		return "Choose A Proper Answer! ";
-	} else if (playerMove === computerMove) {
-		return "It's a Tie!";
-	} else {
-		return "You win! ";
-	}
+    if (playerMove==="paper" && computerMove==="scissors" ||
+    playerMove==="scissors" && computerMove==="rock"||
+    playerMove==="rock" && computerMove==="paper") {
+        
+            return "You Lose! ";
+
+    } 
+    if (playerMove!== "paper" &&
+        playerMove!== "scissors" &&
+        playerMove!== "rock") {
+        
+            return "Choose A Proper Answer! "; 
+    
+    } 
+    if (playerMove===computerMove) {
+            return "It's a Tie!";
+    }
+      if (playerMove==="scissors" && computerMove==="paper" ||
+      playerMove==="paper" && computerMove==="rock"||
+      playerMove==="rock" && computerMove==="scissors") {
+            return "You win! ";
+    }
 }
 const computerChoice = computerPlay();
-
-console.log(
-	game(playerPlay(), computerPlay()) + "Computer Chose " + computerChoice
-);
+const playerChoice = playerPlay();
+console.log(game(playerChoice, computerChoice) + 
+"Computer Chose " + computerChoice);
