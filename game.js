29fe33c648e44720848df78f570fd12f439e90bc
Ahnaf <ahnafanis0@gmail.com>
2022-07-07 19:31:09 +0600

    spacing

--- game.js
+++ game.js
@@ -1,39 +1,41 @@
 "use strict";
+
+//Game Logic
+
 function computerPlay() {
-    const moveset = ["rock", "paper", "scissors"];
-    const move = moveset[(Math.random() * moveset.length)|0];
-    return move;
+	const moveset = ["rock", "paper", "scissors"];
+	const move = moveset[(Math.random() * moveset.length) | 0];
+	return move;
 }
-function playerPlay() { 
-   const question = prompt("Rock, Paper or Scissors?");
-    const move = question.toLowerCase();
-    return move;
+function playerPlay() {
+	const question = prompt("Rock, Paper or Scissors?");
+	const move = question.toLowerCase();
+	return move;
 }
 function game(playerMove, computerMove) {
-    if (playerMove==="paper" && computerMove==="scissors" ||
-    playerMove==="scissors" && computerMove==="rock"||
-    playerMove==="rock" && computerMove==="paper") {
-        
-            return "You Lose! ";
-
-    } 
-    if (playerMove!== "paper" &&
-        playerMove!== "scissors" &&
-        playerMove!== "rock") {
-        
-            return "Choose A Proper Answer! "; 
-    
-    } 
-    if (playerMove===computerMove) {
-            return "It's a Tie!";
-    }
-    if (playerMove==="scissors" && computerMove==="paper" ||
-      playerMove==="paper" && computerMove==="rock"||
-      playerMove==="rock" && computerMove==="scissors") {
-            return "You win! ";
-    }
+	if (
+		(playerMove === "paper" && computerMove === "scissors") ||
+		(playerMove === "scissors" && computerMove === "rock") ||
+		(playerMove === "rock" && computerMove === "paper")
+	) {
+		return "You Lose! ";
+	}
+	if (playerMove !== "paper" && playerMove !== "scissors" && playerMove !== "rock") {
+		return "Choose A Proper Answer! ";
+	}
+	if (playerMove === computerMove) {
+		return "It's a Tie!";
+	}
+	if (
+		(playerMove === "scissors" && computerMove === "paper") ||
+		(playerMove === "paper" && computerMove === "rock") ||
+		(playerMove === "rock" && computerMove === "scissors")
+	) {
+		return "You win! ";
+	}
 }
 const computerChoice = computerPlay();
 const playerChoice = playerPlay();
-console.log(game(playerChoice, computerChoice) + 
-"Computer Chose " + computerChoice);
+console.log(game(playerChoice, computerChoice) + "Computer Chose " + computerChoice);
+
+//Game Logic End
